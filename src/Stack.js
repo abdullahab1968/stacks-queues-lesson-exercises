51392de288166bb4d584e7fab510480a9e3982bb
Abdallah Abu khdair <71616389+abdullahab1968@users.noreply.github.com>
2023-03-11 21:44:38 +0200

    stack implementation

--- src/Stack.js
+++ src/Stack.js
@@ -1,7 +1,50 @@
 /* Write your class below */
 
+class Stack{
+    constructor(){
+        this.stack = []
+        this.length = 0
+    }
+    push(x){
+        this.stack[this.length++] = x
+    }
+    pop(){ 
+        this.length--
+        return this.stack[this.length]
+    }
+    peek(){
+        return this.stack[this.length - 1] || null
+    }
+    isEmpty(){
+        return this.length === 0
+    }
+    print(){
+        let str = '['
+        for(let i =0; i<this.length; i++){
+            str += this.stack[i]
+            if(i < this.length - 1){
+                str += ','
+            }
+        }
+        str += ']'
+        console.log(str)
+    }
+}
 
-
+let myStack = new Stack()
+console.log(myStack.isEmpty())    //true
+myStack.print()                    //[]
+myStack.push(2)      
+console.log(myStack.isEmpty())     //false
+myStack.push(4)      
+myStack.print()                    //[2,4]
+console.log(myStack.peek())        //4
+myStack.pop()
+myStack.pop()
+console.log(myStack.peek())       //null
+console.log(myStack.isEmpty())  
+myStack.pop()
+myStack.print()  
 
 
 
